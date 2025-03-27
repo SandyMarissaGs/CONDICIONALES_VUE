f52507781968eb35454aa29848f13d8b477bfded
SandyMarissaGs <sandymarissa2004@gmail.com>
2025-03-26 22:39:51 -0600

    Agregue importacion del componente recorrido

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Recorrido from './components/Recorrido.vue';
 </script>
 
 <template>
 
-  <HelloWorld msg="Vite + Vue" />
+  <!--<HelloWorld msg="Vite + Vue" />-->
+  <Recorrido/>
 </template>
 
 <style scoped>
