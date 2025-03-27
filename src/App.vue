5ce12bb3704a12372ae2ea14f4b1c97793ff13bf
SandyMarissaGs <sandymarissa2004@gmail.com>
2025-03-26 21:48:41 -0600

    Subiendo Carpeta

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+//import Recorrido from './components/Recorrido.vue';
+import RecorridoPractica from './components/RecorridoPractica.vue';
+</script>
+
+<template>
+
+  <!--<HelloWorld msg="Vite + Vue" />-->
+  <Recorrido/>
+  <RecorridoPractica/>
+</template>
+
+<style scoped>
+
+</style>
