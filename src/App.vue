d6d09b365a80960cc5bafe27d0b94b8c5968b2d0
SandyMarissaGs <sandymarissa2004@gmail.com>
2025-03-30 10:37:48 -0600

    Cambios de creacion de PROPS

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <script setup>
-/*import HelloWorld from './components/HelloWorld.vue'
+import HelloWorl from './components/HelloWorld.vue';
 import RecorridoEjercicio from './components/RecorridoEjercicio.vue';
 import RecorridoEjercio from './components/RecorridoEjercicio.vue';
-import Horario from './components/Horario.vue';*/
+import Horario from './components/Horario.vue';
 import HorarioEjercicio from './components/HorarioEjercicio.vue';
+import HelloWorld11 from './components/HelloWorld11.vue';
+import Padre from './components/Props/Padre.vue';
 </script>
 
 <template>
-  <HorarioEjercicio/>
+  <!--<HorarioEjercicio/>-->
 <!--<Horario/>-->
-  <!--<HelloWorld msg="Vite + Vue" />-->
+  <!--HelloWorld11 Nombre="Sandy Marissa" />--><
  <!---<RecorridoEjercicio/>--> 
+<Padre/>
+
 </template>
 
 <style scoped></style>
---
+++ src/components/HelloWorld11.vue
@@ -0,0 +1,18 @@
+<script setup>
+import{ ref } from 'vue'
+
+
+defineProps({
+ Nombre: String,
+})
+
+
+</script>
+
+<template>
+
+    <h1>{{ Nombre }}</h1>
+ 
+</template>
+
+<style scoped></style>
