d6856618c9fceb5c82a21fbe40ab16df6fdeb45e
SandyMarissaGs <sandymarissa2004@gmail.com>
2025-04-06 21:53:22 -0600

    Cambios de calculadora

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
-import Eventos from './components/Eventos/Eventos.vue';
+import CalculadoraV1 from './components/Calculadora/CalculadoraV1.vue';
+import CalculadoraV2 from './components/Calculadora/CalculadoraV2.vue';
+
+/*import Eventos from './components/Eventos/Eventos.vue';*/
 
 
 </script>
 
 <template>
-  
-  <Eventos/>
+ 
+  <CalculadoraV2/>
 </template>
 
 <style scoped></style>
---
+++ src/components/Calculadora/CalculadoraV1.vue
@@ -0,0 +1,53 @@
+<script>
+export default{
+    data() {
+        return{
+            numero1:0,
+            numero2:0,
+            resultado:0,
+
+        }
+    },
+    methods:{
+        sumar() {
+            this.resultado = parseFloat(this.numero1) + parseFloat(this.numero2);
+        },
+        resta() {
+            this.resultado = parseFloat(this.numero1) - parseFloat(this.numero2);
+        },
+        multiplacion() {
+            this.resultado = parseFloat(this.numero1) * parseFloat(this.numero2);
+        },
+        division() {
+            this.resultado = parseFloat(this.numero1) / parseFloat(this.numero2);
+        }
+    }
+}
+</script>
+<template>
+   <div class="container mt-5">
+    <div class="car shadow p-4">
+        <h3 class="text-center">Calculadora V1</h3>
+        <h2 class="text-center mt-4">Resultado: {{ resultado }} </h2>
+        <div class="mb-3">
+            <label for="">Numero 1</label>
+            <input type="number"class="form-control" v-model="numero1">
+        </div>
+
+        <div class="mb-3">
+            <label for="">Numero 2</label>
+            <input type="number"class="form-control" v-model="numero2">
+        </div>
+
+        <div class="d-flex justify-content-center gap-2">
+        <button @click="sumar" class="btn btn-danger">Sumar</button>
+        <button @click="resta" class="btn btn-warning">Resta</button>
+        <button @click="multiplacion" class="btn btn-info">Multiplicacion</button>
+        <button @click="division" class="btn btn-success">Division</button>
+    </div>
+
+  
+   
+   </div> 
+</div>
+</template>
